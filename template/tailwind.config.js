1b3fada33267d0f81cad70e9890f998f503a401c
Maikul <mcjaiyo01@gmail.com>
2024-08-22 23:04:00 +0300

    custom daisy ui reusable background

--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -3,15 +3,22 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   plugins: [require('daisyui')],
 
-  // daisyUI config (optional - here are the default values)
   daisyui: {
-    themes: ["light", "dark", "cupcake", "retro"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: 'cupcake', // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ':root', // The element that receives theme color CSS variables
+    themes: ['light', 'dark', 'cupcake', 'retro'],
+    darkTheme: 'cupcake',
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: '',
+    logs: true,
+    themeRoot: ':root',
+  },
+  theme: {
+    extend: {
+      backgroundImage: {
+        'theme-gradient':
+          'linear-gradient(135deg, #2D1A45 0%, #3A234F 35%, #522958 70%, #623264 85%, #92405D 100%)',
+      },
+    },
   },
 };
