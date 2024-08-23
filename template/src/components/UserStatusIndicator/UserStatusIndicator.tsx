bd55046fc5d7d67783b8e12d515df10873319ca9
Maikul <mcjaiyo01@gmail.com>
2024-08-24 01:56:27 +0300

    UseStatusindicator component

---
+++ template/src/components/UserStatusIndicator/UserStatusIndicator.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { UserStatus } from '../../enums/UserStatus';
+
+interface UserStatusIndicatorProps {
+  status: UserStatus | undefined;
+  size?: number;
+  absolute?: boolean;
+}
+
+const UserStatusIndicator: React.FC<UserStatusIndicatorProps> = ({
+  status,
+  size = 12,
+  absolute = true,
+}) => {
+  const statusColor = {
+    [UserStatus.ONLINE]: 'bg-green-500',
+    [UserStatus.OFFLINE]: 'bg-gray-500',
+    [UserStatus.BUSY]: 'bg-red-500',
+    [UserStatus.AWAY]: 'bg-yellow-500',
+    [UserStatus.IN_MEETING]: 'bg-blue-500',
+  };
+
+  const userStatusColor = status ? statusColor[status] : 'bg-gray-500';
+  //   absolute bottom-0 right-0 w-3 h-3 rounded-full ${userStatusColor}
+  return (
+    <span
+      className={`rounded-full ${
+        absolute ? 'absolute bottom-0 right-0 w-3 h-3' : ''
+      } ${userStatusColor}`}
+      style={{ width: size, height: size }}
+    />
+  );
+};
+
+export default UserStatusIndicator;
